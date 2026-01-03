437416ece24a79b6baf8c371b3af0b888db3dd12
hhykim <56228907+hhykim@users.noreply.github.com>
2026-01-03 23:53:14 +0900

    fix: 업스트림 사이트 구조 변경 대응

--- notification.py
+++ notification.py
@@ -39,7 +39,11 @@ class Notification:
         self._send_discord_webhook(webhook_url, message)
 
     def make_win720_number_message(self, win720_number: str) -> str:
-        return "\n".join(win720_number.split(","))
+        formatted_numbers = []
+        for number in win720_number.split(","):
+            formatted_number = f"{number[0]}조 " + " ".join(number[1:])
+            formatted_numbers.append(formatted_number)
+        return "\n".join(formatted_numbers)
 
     def send_lotto_winning_message(self, winning: dict, webhook_url: str) -> None: 
         assert type(winning) == dict
@@ -48,7 +52,35 @@ class Notification:
         try: 
             round = winning["round"]
             money = winning["money"]
-            message = f"로또 __***{winning['round']}회***__ - __***{winning['money']}***__ 당첨 :tada:"
+
+            max_label_status_length = max(len(f"{line['label']} {line['status']}") for line in winning["lotto_details"])
+
+            formatted_lines = []
+            for line in winning["lotto_details"]:
+                line_label_status = f"{line['label']} {line['status']}".ljust(max_label_status_length)
+                line_result = line["result"]
+
+                formatted_nums = []
+                for num in line_result:
+                    raw_num = re.search(r"\d+", num).group()
+                    formatted_num = f"{int(raw_num):02d}"
+                    if '✨' in num:
+                        formatted_nums.append(f"[{formatted_num}]")
+                    else:
+                        formatted_nums.append(f" {formatted_num} ")
+
+                formatted_nums = [f"{num:>6}" for num in formatted_nums]
+
+                formatted_line = f"{line_label_status} " + " ".join(formatted_nums)
+                formatted_lines.append(formatted_line)
+
+            formatted_results = "\n".join(formatted_lines)
+
+            if winning["money"] != "-":
+                message = f"로또 __***{winning['round']}회***__ - __***{winning['money']}***__ 당첨 :tada:"
+            else:
+                message = ":money_with_wings: 당첨 내역이 없습니다."
+
             self._send_discord_webhook(webhook_url, message)
         except KeyError:
             message = ":money_with_wings: 당첨 내역이 없습니다."
@@ -61,10 +93,13 @@ class Notification:
         try: 
             round = winning["round"]
             money = winning["money"]
-            message = f"연금복권 __***{winning['round']}회***__ - __***{winning['money']}***__ 당첨 :tada:"
+
+            if winning["money"] != "-":
+                message = f"연금복권 __***{winning['round']}회***__ - __***{winning['money']}***__ 당첨 :tada:"
+
             self._send_discord_webhook(webhook_url, message)
         except KeyError:
-            return
+            pass
 
     def _send_discord_webhook(self, webhook_url: str, message: str) -> None:        
         payload = { "content": message }
